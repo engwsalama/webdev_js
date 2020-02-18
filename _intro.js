7c6c8c854b65f17f1d8eb9d56943c983816caee2
engwsalama <eng.wsalama@gmail.com>
2020-02-18 14:32:26 +0200

    Js intro

---
+++ _intro.js
@@ -0,0 +1,21 @@
+// 1- using Alert and Console
+alert("Hello World!");
+console.log("Hello World!");
+console.error("This is an Error");
+console.warn("This is a Warrning")
+
+/* 2- var & let & const 
+   - we do not use var anymore as it is a global 
+   - Function Scope & block Scope 
+   - var is function scope
+   - let and const are block scope
+   - let & const could not be reassigend but could be mutable
+   {
+       let a=10;
+       console.log(a);
+   }
+   {
+       a++;
+       console.log(a)  ... this block could not recoginze variable a , unless used var
+   }  
+*/

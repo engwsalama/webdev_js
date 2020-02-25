fb873a293fe8b0007d4fd91cfd36bef9906d2a74
engwsalama <eng.wsalama@gmail.com>
2020-02-25 17:46:42 +0200

    update

--- loops-empty.js
+++ loops-empty.js
@@ -15,6 +15,20 @@
 
 
 // MAP
+/* ===> 
+  The map() method creates a new array with the results of calling a function for every array element.
+===>*/
+
+const users = [
+  {id:1,name:'wael'},
+  {id:2,name:'ali'},
+  {id:3,name:'smair'},
+  {id:4,name:'ahmed'},
+];
+const ids=users.map(function(user){
+  return user.id
+});
+console.log(ids)
 
 
 

8ee2d0bf5baf9940ec8defc1a89eb4e0dabcf2fc
engwsalama <eng.wsalama@gmail.com>
2020-02-25 15:17:23 +0200

    event listeners

---
+++ event-listener-empty.js
@@ -0,0 +1,29 @@
+// document.querySelector('.clear-tasks').addEventListener('click', function(e){
+//   console.log('Hello World');
+
+//   //e.preventDefault();
+// });
+
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+function onClick(e){
+  //console.log('Clicked');
+
+  let val;
+
+  val = e;
+
+  // Event target element
+  
+  // Event type
+  
+
+  // Timestamp
+  
+
+  // Coords event relative to the window
+  
+  // Coords event relative to the element
+  
+  console.log(val);
+}
--- replace-remove.js
+++ replace-remove.js
@@ -16,8 +16,8 @@ const cardAction = document.querySelector('.card-action');
 cardAction.replaceChild(newHeading, oldHeading);
 
 // REMOVE ELEMENT
-const lis = document.querySelectorAll('li');
-const list = document.querySelector('ul');
+const lis = document.querySelectorAll('li'); // this for remove element by itself
+const list = document.querySelector('ul');  // this for remove by child
 
 // Remove list item
 lis[0].remove();
@@ -43,7 +43,7 @@ val = link;
 val = link.getAttribute('href');
 val = link.setAttribute('href', 'http://google.com');
 link.setAttribute('title', 'Google');
-val = link.hasAttribute('title');
+val = link.hasAttribute('title'); // return true or false
 link.removeAttribute('title');
 val = link;
 
